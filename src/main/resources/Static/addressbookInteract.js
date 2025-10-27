d5d9b435c90302fabdba6d8e998ff87ea55f10ea
jasonkeah <jasonkeah@cmail.carleton.ca>
2025-10-27 00:15:47 -0400

    fixed up Jquery js and added on click function so (hopefully) JQuery updates div after clicking add

--- src/main/resources/Static/addressbookInteract.js
+++ src/main/resources/Static/addressbookInteract.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-    $ajax({
-        url: "https://personalizedaddressbook-eebqcue0bhayaeft.canadacentral-01.azurewebsites.net/addressBook"
-    }).then(function(data) {
-        $('.addressbook-name').append(data.name);
+    $("#add").on("click", function() {
+        $ajax({
+                url: "https://personalizedaddressbook-eebqcue0bhayaeft.canadacentral-01.azurewebsites.net/addressBook";
+            }).then(function(data) {
+                $('.addressbook-name').append(data.name);
 
-    })
+            });
+    });
 
 
-})
+});
