0b63ac4da623a32550595bf469d5f0a0dcf89781
jasonkeah <jasonkeah@cmail.carleton.ca>
2025-10-27 02:17:20 -0400

    Okayyyy, added function for loading addressbooks

--- src/main/resources/static/js/addressbookInteract.js
+++ src/main/resources/static/js/addressbookInteract.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
+    const apiUrl = "https://personalizedaddressbook-eebqcue0bhayaeft.canadacentral-01.azurewebsites.net/addressBook";
+
+    function loadAddressBooks() {
+            $.ajax({
+                url: apiUrl,
+                type: "GET",
+                success: function(data) {
+                    const list = $(".addressbook-list");
+                    list.empty();
+                    data.forEach(function(book) {
+                        list.append(`<li>${book.name}</li>`);
+                    });
+                },
+                error: function(xhr, status, error) {
+                    console.error("Error fetching address books:", error);
+                }
+            });
+        }
+
+        loadAddressBooks();
+
     $("#create").on("click", function() {
         $.ajax({
-            url: "https://personalizedaddressbook-eebqcue0bhayaeft.canadacentral-01.azurewebsites.net/addressBook",
+            url: apiUrl,
             type: 'POST',
             contentType: 'application/json',
             data: { name: $("#name").val()},
